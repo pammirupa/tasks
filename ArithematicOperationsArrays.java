4011a029ba406aea0821adce2ad4f9dbf58eae33
Rupa <pammirupa@gmail.com>
2023-07-26 17:43:51 +0530

    today's task

--- ArithematicOperationsArrays.java
+++ ArithematicOperationsArrays.java
@@ -1,43 +1,72 @@
-class ArithematicOperationsArrays {
+class ArithmeticOperationsArrays {
     public static void main(String[] args) {
-        int i, j, row = 20, column = 20;
-        int[][] a = new int[row][column];
-        int[][] b = new int[row][column];
-        // Assume arrays a and b have been initialized with appropriate values
+        int row = 3; // Specify the number of rows in the arrays
+        int column = 3; // Specify the number of columns in the arrays
+
+        int[][] a = { {1, 2, 3},
+                      {4, 5, 6},
+                      {7, 8, 9} };
+
+        int[][] b = { {9, 8, 7},
+                      {6, 5, 4},
+                      {3, 2, 8} }; // Note: b[2][2] is set to 0 to test division by zero
 
         int[][] add = new int[row][column];
         int[][] sub = new int[row][column];
         int[][] mul = new int[row][column];
         float[][] div = new float[row][column];
         float[][] mod = new float[row][column];
-
         for (int r = 0; r < row; r++) {
             for (int c = 0; c < column; c++) {
                 add[r][c] = a[r][c] + b[r][c];
                 sub[r][c] = a[r][c] - b[r][c];
                 mul[r][c] = a[r][c] * b[r][c];
-                mod[r][c] = a[r][c] % b[r][c];
 
                 // Check for division by zero before attempting division
                 if (b[r][c] != 0) {
                     div[r][c] = (float) a[r][c] / b[r][c];
-                    mod[r][c] = (int) a[r][c] % b[r][c];
                 } else {
                     // Handle the division by zero case, e.g., set to 0 or display an error message.
                     div[r][c] = 0;
-                    mod[r][c] =0;
-                     // Setting to 0 as an example, you can change this as needed.
                     System.out.println("Division by zero not allowed at [" + r + "][" + c + "]");
                 }
 
-                System.out.println("Addition[" + r + "][" + c + "]: " + add[r][c]);
-                System.out.println("Subtraction[" + r + "][" + c + "]: " + sub[r][c]);
-                System.out.println("Multiplication[" + r + "][" + c + "]: " + mul[r][c]);
-                System.out.println("Division[" + r + "][" + c + "]: " + div[r][c]);
-                System.out.println("Modulus[" + r + "][" + c + "]: " + mod[r][c]);
+                mod[r][c] = a[r][c] % b[r][c];
             }
+        }
+
+        System.out.println("Addition:");
+        printArray(add);
+
+        System.out.println("\nSubtraction:");
+        printArray(sub);
+
+        System.out.println("\nMultiplication:");
+        printArray(mul);
+
+        System.out.println("\nDivision:");
+        printArray(div);
+
+        System.out.println("\nModulus:");
+        printArray(mod);
+    }
+
+    public static void printArray(int[][] arr) {
+        for (int[] row : arr) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printArray(float[][] arr) {
+        for (float[] row : arr) {
+            for (float element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
         }
     }
 }
-
 
